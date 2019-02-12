429ec5b33a257ee899667bfa51c18e53ffd898d7
王亚辉 <>
2019-02-12 20:55:12 +0800

    feat:update code
    Signed-off-by: 王亚辉 <>

--- Runtime_01/Runtime_01/Person.m
+++ Runtime_01/Runtime_01/Person.m
@@ -12,35 +12,41 @@
 
 @implementation Person
 
-//void sendMessage(id self, SEL _cmd, NSString *message) {
-//    NSLog(@"Person--%@", message);
-//}
+void sendMessage(id self, SEL _cmd, NSString *message) {
+    NSLog(@"Person--%@", message);
+}
 
-//+ (BOOL)resolveInstanceMethod:(SEL)sel {
-//    if (sel == @selector(sendMessage:)) {
-//        return class_addMethod(self, sel, (IMP)sendMessage, "v@:@");
-//    }
-//    return NO;
-//}
++ (BOOL)resolveInstanceMethod:(SEL)sel {
+    if (sel == @selector(sendMessage:)) {
+        return class_addMethod(self, sel, (IMP)sendMessage, "v@:@");
+    }
+    return NO;
+}
 
-//- (id)forwardingTargetForSelector:(SEL)aSelector {
-//    if (aSelector == @selector(sendMessage:)) {
-//        return [Student new];
-//    } else {
-//        return [super forwardingTargetForSelector:aSelector];
-//    }
-//}
+- (id)forwardingTargetForSelector:(SEL)aSelector {
+    if (aSelector == @selector(sendMessage:)) {
+        return [Student new];
+    } else {
+        return [super forwardingTargetForSelector:aSelector];
+    }
+}
 
-//- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
-//    if (aSelector == @selector(sendMessage:)) {
-//        return [NSMethodSignature signatureWithObjCTypes:"V@:@"];
-//    }
-//    return nil;
-//}
-//
-//- (void)forwardInvocation:(NSInvocation *)anInvocation {
-//    [anInvocation invokeWithTarget:[Student new]];
-//}
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    if (aSelector == @selector(sendMessage:)) {
+        return [NSMethodSignature signatureWithObjCTypes:"V@:@"];
+    }
+    return nil;
+}
+
+- (void)forwardInvocation:(NSInvocation *)anInvocation {
+    SEL sel = [anInvocation selector];
+    Student *stu = [Student new];
+    if ([stu respondsToSelector:sel]) {
+        [anInvocation invokeWithTarget:stu];
+    } else {
+        [super forwardInvocation:anInvocation];
+    }
+}
 
 - (void)doesNotRecognizeSelector:(SEL)aSelector {
     NSLog(@"doesNotRecognizeSelector");
